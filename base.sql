0a39df3082a127b59ce187e4f5134d717c8ae5df
jairo caro <jairo.caro01@correo.usa.edu.co>
2020-06-04 12:32:33 -0500

    base

---
+++ base.sql
@@ -0,0 +1,26 @@
+create database sergio; 
+use sergio;
+
+create table usuario(
+correo varchar(100) primary key,
+nombre varchar(30),
+clave varchar(10)
+);
+
+create table salones(
+id_salon varchar(20) primary key,
+capacidad integer,
+tipo varchar(30)
+);
+
+create table reserva(
+id_reserva varchar(10) primary key,
+correo varchar(100),
+id_salon varchar(20),
+foreign key (id_salon) references salones(id_salon),
+foreign key (correo) references usuario(correo),
+dia_reserva date,
+hora_resrva time,
+tema varchar(50)
+);
+
