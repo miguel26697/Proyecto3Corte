738d8ec7f218afdf2cb27f60ba23c9d5f13125d6
unknown <sacali99@gmail.com>
2020-06-06 10:13:30 -0500

    agrego una barra buscadora para el foro

--- js/foro.js
+++ js/foro.js
@@ -1,5 +1,39 @@
+var url = "http://ec2-34-209-88-143.us-west-2.compute.amazonaws.com";
 var img="";
 var texto="";
+
+function get_categoria(){
+    $.ajax({
+        type : 'GET',
+        url: url+"/Categories",
+        headers:{
+            usaHeaders: token
+        },
+        datType:"JSON",
+        success: function(rta){
+            datos_categoria(rta);
+        }
+    })
+}
+
+function AddCategory(non, des, log){
+	$("#target").empty();
+	token=getCookie("token");
+	$.ajax({
+		url: url + "/Category",
+		type : 'POST',
+		data : JSON.stringify({"name": non, "description":des, "logo":log}),
+		headers: {
+			usaHeader : token
+		},
+		contenType : "JSON",
+		dataType: "JSON",
+		success: function(rta){
+			console.log(rta);
+		}
+	});
+}
+
 
 function datos_categoria(rta){
     for(var i =0; i<rta.length; i++){
@@ -15,17 +49,27 @@ function datos_categoria(rta){
     }
 }
 
-function get_categoria(){
-    $.ajax({
-        type : 'GET',
-        url: url+"/Categories",
-        headers:{
-            usaHeaders: token
-        },
-        datType:"JSON",
-        success: function(rta){
-            datos_categoria(rta);
-        }
-    })
-}
+
+var consulta = $("#searchTable").DataTable();
+
+$("#inputBusqueda").keyup(function(){
+	consulta.search($(this).val()).draw();
+
+	$("header").css({
+		"height": "100vh",
+		"background": "rgba(0,0,0,0.5)"
+	})
+
+	if ($("#inputBusqueda").val() == ""){
+		$("header").css({
+			"height": "auto",
+			"background": "none"
+		})
+
+		$("#search").hide()
+
+	} else {
+		$("#search").fadeIn("fast");
+	}
+})
 
