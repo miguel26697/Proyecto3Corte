5ef3ce411dc82398fe4ad5b8d0ea9f486fe842ed
unknown <sacali99@gmail.com>
2020-06-08 11:09:35 -0500

    realizo entrada al primer foro

--- js/foro.js
+++ js/foro.js
@@ -1,87 +1,74 @@
 var url = "http://ec2-34-209-88-143.us-west-2.compute.amazonaws.com";
-var img="";
-var texto="";
+var img = "";
+var texto = "";
 const formulario = document.querySelector('#formulario');
 const boton = document.querySelector('#boton');
 const resultado = document.querySelector('#resultado');
 
-const filtar = ()=>{
+const filtar = () => {
 	//console.log(formulario.value);
 	resultado.innerHTML = '';
 
 	const texto = formulario.value.toLowerCase();
 
-	for(let foro of foros ){
+	for (let foro of foros) {
 		let nombre = foro.nombre.toLowerCase();
-		if(nombre.indexOf(texto) !== -1){
+		if (nombre.indexOf(texto) !== -1) {
 			resultado.innerHTML += `
 			<li>${foro.nombre}</li>	
 			`
-		}	
+		}
 	}
-	if(resultado.innerHTML== ''){
+	if (resultado.innerHTML == '') {
 		resultado.innerHTML += `
 			<li> Foro no encontrado....</li>
 		`
 	}
-} 
-boton.addEventListener('click',filtar)
+}
+boton.addEventListener('click', filtar)
 
-const foros =[
-	{nombre: 'Ciencias aplicladas'},
-	{nombre: 'Ciencias politicas'},
-	{nombre: 'Idiomas'},
-	{nombre: 'Artes'},
-	{nombre: 'Derecho'},
-	{nombre: 'progremación'},
+const foros = [
+	{ nombre: 'Ciencias aplicladas' },
+	{ nombre: 'Ciencias politicas' },
+	{ nombre: 'Idiomas' },
+	{ nombre: 'Artes' },
+	{ nombre: 'Derecho' },
+	{ nombre: 'progremación' },
 ]
 
-function get_categoria(){
-    $.ajax({
-        type : 'GET',
-        url: url+"/Categories",
-        headers:{
-            usaHeaders: token
-        },
-        datType:"JSON",
-        success: function(rta){
-            datos_categoria(rta);
-        }
-    })
+function get_categoria() {
+	$.ajax({
+		type: 'GET',
+		url: url + "/Categories",
+		headers: {
+			usaHeaders: token
+		},
+		datType: "JSON",
+		success: function (rta) {
+			datos_categoria(rta);
+		}
+	})
 }
 
-function AddCategory(non, des, log){
+function AddCategory(non, des, log) {
 	$("#target").empty();
-	token=getCookie("token");
+	token = getCookie("token");
 	$.ajax({
 		url: url + "/Category",
-		type : 'POST',
-		data : JSON.stringify({"name": non, "description":des, "logo":log}),
+		type: 'POST',
+		data: JSON.stringify({ "name": non, "description": des, "logo": log }),
 		headers: {
-			usaHeader : token
+			usaHeader: token
 		},
-		contenType : "JSON",
+		contenType: "JSON",
 		dataType: "JSON",
-		success: function(rta){
+		success: function (rta) {
 			console.log(rta);
 		}
 	});
 }
 
 
-function datos_categoria(rta){
-    for(var i =0; i<rta.length; i++){
-        img = rta[i].logo;
-		texto ='<div class="card">'+
-		'<div class="image">'+
-		'<img src="'+ img +'">'+
-		'</div>'+
-		'<div class="content">'+
-		'<div class="header">'+rta[i].name+'</div>'+
-		'<div class="description">'+rta[i].description+'</br></div>';
-		$("#target").append(texto);
-    }
-}
 
 
 
